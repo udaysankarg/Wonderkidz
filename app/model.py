70dfa6063942b92e2b621baf810191ac5da276d8
Uday Sankar G <udaysankarg@gmail.com>
2020-05-10 19:46:51 +0530

    Added application

--- app/model.py
+++ app/model.py
@@ -1,51 +1,58 @@
 import logging
-import time
-import mysql.connector
-import os
+
+from db.dbutils import *
 
 logger = logging.getLogger(__name__)
 
+from datetime import datetime, date
 
-def setup():
-    config = {
-        'host': os.getenv("MYSQL_HOST"),
-        'user': os.getenv("MYSQL_USER"),
-        'passwd': os.getenv("MYSQL_PASSWORD"),
-        'database': os.getenv("MYSQL_DATABASE")
-    }
-    return config
 
-def getconn():
-    config = setup()
-    retries = 5
-    while retries:
-        try:
-            mydb = mysql.connector.connect(**config)
-            return mydb
-        except :
-            retries -= 1
-            logger.error("Connection failed.. Retrying after 1 sec")
-            time.sleep(1)
-
+def dbsetup():
+    try:
+        cnx = getconn()
+        create_database(cnx)
+        print("Database created")
+    except:
+        print("Error in creating DB")
+        exit(1)
 
 
 def getdbs():
     mydb = getconn()
     mycursor = mydb.cursor()
 
-    mycursor.execute("CREATE DATABASE IF NOT EXISTS wonderkidz")
-    mycursor.execute("CREATE TABLE IF NOT EXISTS `EMPLOYEES` (`id` INT AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR(255), `address` VARCHAR(255))")
     mycursor.execute("SHOW DATABASES;")
     dbs = mycursor.fetchall()
     dbs = [x[0] for x in dbs]
     mydb.close()
     return dbs
 
+
 def get_emp_list():
     mydb = getconn()
-    mycursor = mydb.cursor()
-    mycursor.execute("select * from wonderkidz.EMPLOYEES;")
-    dbs = mycursor.fetchall()
-    dbs = [x[0] for x in dbs]
+    dbs = get_data(mydb)
+    emplist = []
+    for v in dbs:
+        empdata = {}
+        empdata['empid'] = v[0]
+        empdata['fname'] = v[1]
+        empdata['lname'] = v[2]
+        empdata['hdate'] = datetime.strftime(v[3], "%d-%m-%Y")
+        emplist.append(empdata)
     mydb.close()
-    return dbs
+    return emplist
+
+
+def insert_emp(mem_info):
+    mydb = getconn()
+    if not mem_info:
+        data = ('Uday', 'Sankar', datetime.now().date(), 'M', date(1989, 3, 10))
+    else:
+        data = (
+            mem_info['fname'], mem_info['lname'], datetime.strptime(mem_info['doj'], "%d-%m-%Y").date(), mem_info['gender'],
+        datetime.strptime(mem_info['dob'], "%d-%m-%Y").date())
+        # first_name, last_name, hire_date, gender, birth_date
+
+    logger.info("Emp added:", insertemp(mydb, data))
+    mydb.close()
+    return
